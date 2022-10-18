b40bf01b72079f4336ba0ac3f3738c1eaca70fce
Esteferson Quadros <esteferson.quadros@gmail.com>
2022-10-18 00:26:10 -0300

    Update projeto-iac.sh
    Alteração das senhas.

--- projeto-iac.sh
+++ projeto-iac.sh
@@ -12,17 +12,17 @@ groupadd GRP_VEN
 groupadd GRP_SEC
 
 echo "USUÁRIOS:"
-useradd carlos -m -s /bin/bash -p $(openssl passwd -crypt 1529051994) -G GRP_ADM
-useradd maria -m -s /bin/bash -p $(openssl passwd -crypt 1529051994) -G GRP_ADM
-useradd joao -m -s /bin/bash -p $(openssl passwd -crypt 1529051994) -G GRP_ADM
+useradd carlos -m -s /bin/bash -p $(openssl passwd -crypt 152905) -G GRP_ADM
+useradd maria -m -s /bin/bash -p $(openssl passwd -crypt 152905) -G GRP_ADM
+useradd joao -m -s /bin/bash -p $(openssl passwd -crypt 152905) -G GRP_ADM
 
-useradd debora -m -s /bin/bash -p $(openssl passwd -crypt 1529051994) -G GRP_VEN
-useradd sebastiana -m -s /bin/bash -p $(openssl passwd -crypt 1529051994) -G GRP_VEN
-useradd roberto -m -s /bin/bash -p $(openssl passwd -crypt 1529051994) -G GRP_VEN
+useradd debora -m -s /bin/bash -p $(openssl passwd -crypt 152905) -G GRP_VEN
+useradd sebastiana -m -s /bin/bash -p $(openssl passwd -crypt 152905) -G GRP_VEN
+useradd roberto -m -s /bin/bash -p $(openssl passwd -crypt 152905) -G GRP_VEN
 
-useradd josefina -m -s /bin/bash -p $(openssl passwd -crypt 1529051994) -G GRP_SEC
-useradd amanda -m -s /bin/bash -p $(openssl passwd -crypt 1529051994) -G GRP_SEC
-useradd rogerio -m -s /bin/bash -p $(openssl passwd -crypt 1529051994) -G GRP_SEC
+useradd josefina -m -s /bin/bash -p $(openssl passwd -crypt 152905) -G GRP_SEC
+useradd amanda -m -s /bin/bash -p $(openssl passwd -crypt 152905) -G GRP_SEC
+useradd rogerio -m -s /bin/bash -p $(openssl passwd -crypt 152905) -G GRP_SEC
 
 echo "PERMISSÕES:"
 chown root:GRP_ADM /adm
